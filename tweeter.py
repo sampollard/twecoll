a421a69cf4ae6b3b00718f756f80cbc6cceb2595
Samuel Pollard <sam.d.pollard@gmail.com>
2018-02-23 14:36:47 -0800

    Fixed tweeter to display unicode and &s

--- tweeter.py
+++ tweeter.py
@@ -1,13 +1,19 @@
 import random
 import sys
 import pandas as pd
-eols = ['.', '?', ';']
-digit = ['0','1','2','3','4','5','6','7','8','9','0']
+import HTMLParser
+#from bs4 import BeautifulSoup
+# Suppress warnings for reading URL in a tweet
+#import warnings
+#warnings.filterwarnings("ignore", category=UserWarning, module='bs4')
+eols = [u'.', u'?', u';']
+digit = [u'0',u'1',u'2',u'3',u'4',u'5',u'6',u'7',u'8',u'9',u'0']
 
 triples = {}
 if len(sys.argv) != 2:
     print("usage: tweeter.py <training_corpus>. \
-    This should be a csv with least the column 'text'")
+        This should be a csv with at least the column 'text'")
+    sys.exit(2)
 fn = sys.argv[1]
 
 df = None
@@ -24,7 +30,10 @@ if 'is_retweet' in df.columns:
     tweets = df['text'][df['is_retweet'] == False]
 else:
     tweets = df['text']
-words = '\n'.join(tweets).split()
+h = HTMLParser.HTMLParser()
+tweets = tweets.apply(lambda s: h.unescape(s.decode('utf-8')))
+
+words = u'\n'.join(tweets).split()
 
 # Generate a 4-gram model
 for i in range(len(words)-3):
@@ -38,10 +47,10 @@ NG = len(triples)
 for _ in range(15):
     l = random.choice(triples.keys())
     w = random.choice(triples[l])
-    twt = "{} {} {}".format(*l) + ('\n' if w[-1] in eols else '')
+    twt = u"{} {} {}".format(*l) + (u'\n' if w[-1] in eols else '')
     while len(twt) < 140:
         w = random.choice(triples[l])
-        twt = twt + " {0}".format(w) + ('\n' if w[-1] in eols else '')
+        twt = twt + u" {0}".format(w) + (u'\n' if w[-1] in eols else '')
         if len(twt) >= 40 and w[-1] in eols:
             break # That's a long enough tweet
         l = (l[1], l[2], w)
